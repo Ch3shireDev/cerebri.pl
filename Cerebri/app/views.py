0af170a9ac154568f420c62560556997877577e8
Cheshire Cat <thesmilingcatofcheshire@gmail.com>
2019-08-04 21:18:07 +0200

    Expanded edit_exercise and edit_test pages

--- Cerebri/app/views.py
+++ Cerebri/app/views.py
@@ -8,6 +8,7 @@ from django.template.defaulttags import register
 from app.models import webpage_dict, Exercise, Test, AnswerType, AnswerDictionary
 from django.http import HttpResponse
 import random
+import json
 
 @register.filter
 def get_item(dictionary, key):
@@ -20,12 +21,43 @@ def shuffle(arg):
     return tmp
 
 def home_view(request):
-    return render(request, 'app/index.html')
+    tests = Test.objects.all()
+    return render(request, 'app/index.html', context={'tests': tests})
 
 def test_view(request, test_url):
     test = Test.objects.get(url=test_url)
     exercise_url = test.get_first_exercise()
     return redirect('%s/%s' % (test_url, exercise_url))
+
+def exercise_render(request,title, content, answers,answer_type,test_url=None,exercise_url=None,next_url=None,previous_url=None,points=None,total_points=None):
+   
+    
+    if points is not None:
+        if points == 1: 
+            points_text = "1 punkt"
+        elif points in [2,3,4]:
+            points_text = "%d punkty" % points
+        else:
+            points_text = "%d punktów" % points
+    else:
+        points_text=None
+
+        
+    context = {'exercise_title': title,
+               'exercise_content': content,
+               'exercise_answers': answers,
+               'next_url': next_url,
+               'previous_url': previous_url,
+               'points_text': points_text,
+               'exercise_points': points,
+               'exercise_url': exercise_url,
+               'total_points': total_points,
+               'test_url': test_url,
+               'exercise_url': exercise_url,
+               }
+
+    webpage = webpage_dict[str(answer_type)] #html source
+    return render(request, webpage, context=context)
 
 def exercise_view(request, test_url, exercise_url):
     
@@ -40,29 +72,9 @@ def exercise_view(request, test_url, exercise_url):
 
     previous_url = test.get_previous_exercise(exercise_url)
     points = exercise.points
-
-    if points == 1: 
-        points_text = "1 punkt"
-    elif points in [2,3,4]:
-        points_text = "%d punkty" % points
-    else:
-        points_text = "%d punktów" % points
-
     total_points = test.get_total_points()
 
-    context = {'exercise_title': title,
-               'exercise_content': content,
-               'exercise_answers': answers,
-               'next_url': next_url,
-               'previous_url': previous_url,
-               'points_text': points_text,
-               'exercise_points': points,
-               'exercise_id': exercise,
-               'total_points': total_points
-               }
-
-    webpage = webpage_dict[str(answer_type)] #html source
-    return render(request, webpage, context=context)
+    return exercise_render(request,title,content,answers,answer_type,test_url, exercise_url,next_url,previous_url,points,total_points)
 
 
 
@@ -70,7 +82,6 @@ def exercise_view(request, test_url, exercise_url):
 
 @staff_member_required
 def exercise_edit_view(request, test_url, exercise_url):
-    
     exercise = Exercise.objects.get(url=exercise_url)
     test = Test.objects.get(url=test_url)
     
@@ -101,7 +112,8 @@ def exercise_edit_view(request, test_url, exercise_url):
         'previous_url': previous_url,
         'points_text': points_text,
         'exercise_points': points,
-        'exercise_id': exercise,
+        'exercise_url': exercise_url,
+        'test_url': test_url,
         'total_points': total_points,
         'answer_type': answer_type,
         'answer_types': answer_types,
@@ -109,14 +121,66 @@ def exercise_edit_view(request, test_url, exercise_url):
                }
 
 
-    return render(request, 'app/edit.html', context)
+    return render(request, 'app/edit_exercise.html', context)
 
+@staff_member_required
 def exercise_edit_render(request, test_url, exercise_url):
     if request.method != 'POST':
         return HttpResponse(status=500)
 
     title = request.POST['title']
     content = request.POST['content']
+    answer_type = request.POST['answer_type']
+    answers = request.POST['answers']
+    answers = eval(answers)
 
+    answer_type = AnswerType(int(answer_type))
+    return exercise_render(request,title,content,answers,answer_type)
 
+@staff_member_required
+def exercise_edit_save(request,test_url,exercise_url):
+    if request.method != 'POST':
+        return HttpResponse(status=500)
+
+    title = request.POST['title']
+    content = request.POST['content']
+    answer_type = request.POST['answer_type']
+    answers = request.POST['answers']
+    points = request.POST['points']
+
+    try:
+        points = int(points)
+    except:
+        points = 0
+
+    exercise = Exercise.objects.get(url=exercise_url)
+
+    exercise.title = title
+    exercise.content = content
+    exercise.answer_type = int(answer_type)
+    exercise.answers = answers
+    exercise.points = points
+
+    return HttpResponse(status=200)
+
+@staff_member_required
+def test_edit_view(request, test_url):
+    test = Test.objects.get(url=test_url)
+    return render(request, 'app/edit_test.html', {'test':test, 'test_url':test_url })
+
+@staff_member_required
+def test_edit_show(request, test_url):
+    return HttpResponse(status=200)
+
+@staff_member_required
+def test_edit_save(request,test_url):
+    return HttpResponse(status=200)
+
+@staff_member_required
+def exercise_add_before(request, test_url, exercise_url):
+
+    return HttpResponse(status=200)
+
+@staff_member_required
+def exercise_add_after(request,test_url, exercise_url):
     return HttpResponse(status=200)
--- Cerebri/system/urls.py
+++ Cerebri/system/urls.py
@@ -9,14 +9,20 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.shortcuts import redirect
 
-urlpatterns = [
-                  path('', views.home_view),
-                  path('<test_url>', views.test_view),
-                  path('<test_url>/<exercise_url>', views.exercise_view),
-                  path('<test_url>/<exercise_url>/edit', views.exercise_edit_view),
-                  path('<test_url>/<exercise_url>/render', views.exercise_edit_render),
-                  path('admin/', admin.site.urls),
-              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-#+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-#+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
+urlpatterns = [                  
+                path('', views.home_view),
+                path('admin', admin.site.urls),
+                #path('admin/', admin.site.urls),
+                path('<test_url>', views.test_view),
+                path('<test_url>/edit', views.test_edit_view),
+                path('<test_url>/view', views.test_edit_show),
+                path('<test_url>/save', views.test_edit_save),
+                path('<test_url>/<exercise_url>', views.exercise_view),
+                path('<test_url>/<exercise_url>/edit', views.exercise_edit_view),
+                path('<test_url>/<exercise_url>/view', views.exercise_edit_render),
+                path('<test_url>/<exercise_url>/save', views.exercise_edit_save),
                 
+                path('<test_url>/<exercise_url>/add-before', views.exercise_add_before),
+                path('<test_url>/<exercise_url>/add-after', views.exercise_add_after),
+
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
